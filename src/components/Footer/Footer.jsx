82cea2aa2e5611584d5ca121b4183f7f5cce58ab
ongchunheng <ocheng.90@gmail.com>
2022-11-28 21:10:30 +0800

    Added footer link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
+  const navigate = () => {
+    window.open("https://ongch24014.github.io/portfolio/");
+  };
   return (
     <footer>
-        <span className='boxed'>Created by Chun Heng</span>
+      <span className="boxed" onClick={navigate}>
+        Created by Chun Heng
+      </span>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
